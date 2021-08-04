954a2dad610b72194b73fe3f98c48514e6048319
Mense70056 <88414829+Mense70056@users.noreply.github.com>
2021-08-04 08:34:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def time():
 	label.after(1000, time)
 
 label = Label(root, font=("ds-digital",80), background = "black", foreground = "cyan")
-label.pack(anchor= 'center')
-time()
+label.pack(anchor= 'center'); time()
 
 mainloop()
